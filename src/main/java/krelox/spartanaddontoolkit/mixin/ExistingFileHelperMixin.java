93595bd2e526eb76ff7e1c33f61ca09267d2257f
KreloX <krelox@seznam.cz>
2024-06-25 21:23:45 +0200

    feat: make existing file checks skip spartan weaponry files

---
+++ src/main/java/krelox/spartanaddontoolkit/mixin/ExistingFileHelperMixin.java
@@ -0,0 +1,22 @@
+package krelox.spartanaddontoolkit.mixin;
+
+import com.oblivioussp.spartanweaponry.ModSpartanWeaponry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ExistingFileHelper.class)
+public class ExistingFileHelperMixin {
+    @Inject(
+            method = "exists(Lnet/minecraft/resources/ResourceLocation;Lnet/minecraftforge/common/data/ExistingFileHelper$IResourceType;)Z",
+            at = @At("RETURN"),
+            cancellable = true,
+            remap = false
+    )
+    private void exists(ResourceLocation loc, ExistingFileHelper.IResourceType type, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(cir.getReturnValue() || loc.getNamespace().equals(ModSpartanWeaponry.ID));
+    }
+}
