f548dda41cf8fa65617b6ad51990b991e3a33df4
KreloX <krelox@seznam.cz>
2024-06-27 14:53:33 +0200

    feat: store weapons in a LinkedHashMap for ordered iteration

--- src/main/java/krelox/spartantoolkit/WeaponMap.java
+++ src/main/java/krelox/spartantoolkit/WeaponMap.java
@@ -4,9 +4,9 @@ import it.unimi.dsi.fastutil.Pair;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
-public class WeaponMap extends HashMap<Pair<SpartanMaterial, WeaponType>, RegistryObject<Item>> {
+public class WeaponMap extends LinkedHashMap<Pair<SpartanMaterial, WeaponType>, RegistryObject<Item>> {
     public WeaponMap() {
         super();
     }
